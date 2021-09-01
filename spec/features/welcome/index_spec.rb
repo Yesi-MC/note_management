b3d8a57f6b6c6330b47c4e85c387c5b1f09ed174
Yesi Meza <yesi.meza10@gmail.com>
2021-09-01 15:13:23 -0600

    creates test for user login

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -9,5 +9,16 @@ RSpec.describe "as a user" do
       expect(page).to have_field("password")
       expect(page).to have_button("Login")
     end
+    it "allows user to user to login to dashboard" do 
+      user1 = User.create!(name: "Joe User", email: "joeuser@gmail.com", password: "pass123")
+
+      visit root_path
+
+      fill_in :email, with: user1.email 
+      fill_in :password, with: user1.password 
+      click_button "Login"
+
+      expect(current_path).to eq(dashboard_path(user1.id)) 
+    end
   end
 end
