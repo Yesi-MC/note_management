02e4c6f66e670cbb5bd9a1961b39106b3a74a60d
Yesi Meza <yesi.meza10@gmail.com>
2021-09-01 19:51:00 -0600

    create test to see welcome message on root_path

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "as a user" do 
   describe "when I visit '/'" do 
+    it "should see a welcome message" do 
+      visit root_path 
+
+      expect(page).to have_content("Welcome to Note Management")
+    end
     it "should see a button to login user" do 
       visit root_path
 
