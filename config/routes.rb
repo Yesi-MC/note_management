96c70f938545d314ef8427263bddf73ba8b56c12
Yesi Meza <yesi.meza10@gmail.com>
2021-09-02 12:35:15 -0600

    added route for create action

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,5 @@ Rails.application.routes.draw do
   resources :users, only: [:new]
     get '/users/:id/dashboard', to: 'users/dashboard#index', as: :dashboard
     get '/users/:id/note/new', to: 'users/notes#new', as: :new_user_note
-    
     post '/users/:id/note/new', to: 'users/notes#create'
-    # resources :notes, only: [:create]
 end
