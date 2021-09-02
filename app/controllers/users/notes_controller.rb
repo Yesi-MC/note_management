6853f89eb2a9d4ab36a5283e1ccb04548f67ea52
Yesi Meza <yesi.meza10@gmail.com>
2021-09-02 12:36:59 -0600

    added new and create action in controller

--- app/controllers/users/notes_controller.rb
+++ app/controllers/users/notes_controller.rb
@@ -1,7 +1,25 @@
 class Users::NotesController < ApplicationController 
 
-  def create 
-    # require 'pry'; binding.pry
+  def new 
+    @note = Note.new
   end
- 
+
+  def create 
+    @user = User.find(params[:id])
+    @note = @user.notes.new(note_params)
+
+    if @note.save
+    flash[:notice] = "#{@note.title} was created"
+    redirect_to dashboard_path(current_user)
+    else 
+      flash[:notice] = "Unable to create note"
+      render :new
+    end
+  end
+
+  private 
+
+  def note_params 
+    params.require(:note).permit(:title, :body)
+  end
 end
