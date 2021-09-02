6c4a5bbd8ebc5d98acedb61b0e37af9388ff4abf
Yesi Meza <yesi.meza10@gmail.com>
2021-09-01 20:08:56 -0600

    removed commented out pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
   def login
-    # require 'pry'; binding.pry
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
        session[:user_id] = user.id 
