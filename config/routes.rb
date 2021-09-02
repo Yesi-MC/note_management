8039ab831e38f36612d997a8ecc8bef2a4faef78
Yesi Meza <yesi.meza10@gmail.com>
2021-09-01 19:52:59 -0600

    added logout  path to route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   get '/login', to: 'sessions#login'
+  get '/logout', to: 'sessions#logout'
 
   resources :users, only: [:new]
     get '/users/:id/dashboard', to: 'users/dashboard#index', as: :dashboard
