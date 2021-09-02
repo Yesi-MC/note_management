47850a1ebe5674a31125741bace12ce5b658426b
Yesi Meza <yesi.meza10@gmail.com>
2021-09-02 12:37:34 -0600

    removed created_at validation

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -8,6 +8,5 @@ RSpec.describe Note, type: :model do
   describe 'validations' do 
     it {should validate_presence_of :title}
     it {should validate_presence_of :body}
-    it {should validate_presence_of :created_at}
   end
 end
